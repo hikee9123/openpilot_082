6e67a1fa3c0aed80d71ce80e91f55dee704f9020
hikee9123 <hikee9123@gmail.com>
2021-02-21 17:28:09 +0900

    dd

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -456,8 +456,6 @@ class CarInterface(CarInterfaceBase):
     return self.CS.out
 
   def apply(self, c, sm, CP):
-    can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators,
-                               c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
-                               c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart)
+    can_sends = self.CC.update(c, self.CS, self.frame, sm, CP )
     self.frame += 1
     return can_sends
