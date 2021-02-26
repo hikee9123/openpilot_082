94a7b38df1bb35157849129bc163d7ab1059bb17
hikee9123 <hikee9123@gmail.com>
2021-02-26 21:35:42 +0900

    dd

--- selfdrive/car/hyundai/spdcontroller.py
+++ selfdrive/car/hyundai/spdcontroller.py
@@ -146,8 +146,8 @@ class SpdController():
     def calc_va(self, sm, v_ego):
         md = sm['modelV2']
         #print('{}'.format( md.position.x ) )
-        if len(md.position.t) > 1:
-            self.prob = md.position.t
+        if len(md.laneLines[1].y) > 1:
+            self.prob = md.laneLines[1].y
             #self.prob = md.curv
 
             model_speed = self.calc_laneProb( self.prob, v_ego )
