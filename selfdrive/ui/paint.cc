cda6742b32440b08244152a1e0322d002ba0ecf9
hikee9123 <hikee9123@gmail.com>
2021-02-26 21:21:54 +0900

    dd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -316,13 +316,11 @@ static void ui_draw_debug(UIState *s)
   int  x_pos = 100+250; 
 
 
-  //float  gyroBias = scene.liveParameters.getGyroBias();
+
   float  angleOffset = scene.liveParameters.getAngleOffsetDeg();
   float  angleOffsetAverage = scene.liveParameters.getAngleOffsetAverageDeg();
   float  stiffnessFactor = scene.liveParameters.getStiffnessFactor();
   float  steerRatio = scene.liveParameters.getSteerRatio();
-  //float  yawRate = scene.liveParameters.getYawRate();
-  //float  posenetSpeed = scene.liveParameters.getPosenetSpeed();
 
 
   float  planSteerRatio = scene.lateralPlan.getSteerRatio();
@@ -332,14 +330,11 @@ static void ui_draw_debug(UIState *s)
   float  cpuPerc = scene.deviceState.getCpuUsagePercent();
 
 
-
-
-  //int standstill = scene.car_state.getStandstill();
   
   auto cruiseState = scene.car_state.getCruiseState();  
   int modeSel = cruiseState.getModeSel();
   float model_speed = scene.controls_state.getModelSpeed();
-  float curvature = scene.controls_state.getCurvature();
+ // float curvature = scene.controls_state.getCurvature();
 
   nvgTextAlign(s->vg, NVG_ALIGN_LEFT | NVG_ALIGN_BASELINE);
   if( scene.dash_menu_no  )  
@@ -353,7 +348,7 @@ static void ui_draw_debug(UIState *s)
 
     ui_print( s, x_pos, y_pos+0,   "sR:%.2f , %.2f  Fan:%.0f", steerRatio, planSteerRatio, fanSpeed/1000. );
     ui_print( s, x_pos, y_pos+50,  "aO:%.2f, %.2f", angleOffset, angleOffsetAverage );
-    ui_print( s, x_pos, y_pos+100, "sF:%.2f CV:%.0f, %.5f", stiffnessFactor, model_speed, curvature );
+    ui_print( s, x_pos, y_pos+100, "sF:%.2f CV:%.0f", stiffnessFactor, model_speed );
 
 
     //ui_print( s, x_pos, y_pos+200, "prob:%.2f, %.2f, %.2f, %.2f", scene.lane_line_probs[0], scene.lane_line_probs[1], scene.lane_line_probs[2], scene.lane_line_probs[3] );
