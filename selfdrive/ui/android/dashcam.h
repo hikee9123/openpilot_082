0da59d50c75aecd1688d7e5192cb0fcb097fcaa8
hikee9123 <hikee9123@gmail.com>
2021-03-07 19:08:09 +0900

    dd

--- selfdrive/ui/android/dashcam.h
+++ selfdrive/ui/android/dashcam.h
@@ -444,10 +444,10 @@ static void screen_menu_button(UIState *s, int touch_x, int touch_y, int touched
     sprintf( szText, "%d", scene.dash_menu_no );
     nvgText(s->vg, btn_x - 50, btn_y + 50, szText, NULL);
 
-    if( scene.dash_menu_no )
+    if( scene.dash_menu_no == 2 )
     {
         const int bb_dmr_w = 300;
-        const int bb_dmr_x = s->viz_rect.x + (s->viz_rect.w - 200) - bb_dmr_w - (bdr_s * 2);
+        const int bb_dmr_x = s->viz_rect.x + (s->viz_rect.w * 0.95) - bb_dmr_w - (bdr_s * 2);
         const int bb_dmr_y = (bdr_s + (bdr_s * 1.5)) + 220;
 
         draw_menu( s, bb_dmr_x, bb_dmr_y, bb_dmr_w );
