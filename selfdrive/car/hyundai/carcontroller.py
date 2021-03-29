73ccde433723e59f23284cc5ba5e1c9c9a73993a
hikee9123 <hikee9123@gmail.com>
2021-03-29 22:50:40 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -115,7 +115,7 @@ class CarController():
 
 
 
-  def atom_tune( self, v_ego_kph, cv_value ):  # cV(����� ���� ��ȭ)
+  def atom_tune( self, v_ego_kph, cv_value ):  # cV
     self.cv_KPH = self.CP.atomTuning.cvKPH
     self.cv_BPV = self.CP.atomTuning.cvBPV
     self.cv_sMaxV  = self.CP.atomTuning.cvsMaxV
@@ -164,34 +164,29 @@ class CarController():
       param.STEER_DELTA_UP = min( param.STEER_DELTA_UP, nUP)
       param.STEER_DELTA_DOWN = min( param.STEER_DELTA_DOWN, nDN )
 
-    sec_mval = 5  # 오파 => 운전자.  (sec)
-    sec_pval = 3 #  운전자 => 오파  (sec)
-    # streer over check
 
 
-    if path_plan.laneChangeState != LaneChangeState.off:
-      self.steer_torque_over_timer = 0
-    elif CS.out.leftBlinker or CS.out.rightBlinker:
-      sec_mval = 0.5 # 오파 => 운전자.
-      sec_pval = 10  # 운전자 => 오파  (sec)
+    if abs(CS.out.steeringAngleDeg) >= CS.CP.maxSteeringAngleDeg:
+      sec_mval = 0.5  # 오파 => 운전자.  (sec)
+      sec_pval = 10 #  운전자 => 오파  (sec)
+      self.timer1.startTime( 5000 )
+    elif self.timer1.endTime():
+      sec_mval = 5  # 오파 => 운전자.  (sec)
+      sec_pval = 3 #  운전자 => 오파  (sec)
 
-    if v_ego_kph > 5 and CS.out.steeringPressed and CS.out.cruiseState.enabled:
-      if abs_angle_steers > 5 and CS.out.steeringTorque < -10:   #right
-        if dst_steer < 0:
-          self.steer_torque_over_timer = 0
-        else:
-          #sec_mval = 1
-          self.steer_torque_over_timer = 50
-      elif abs_angle_steers > 5 and CS.out.steeringTorque > 10:  #left
-        if dst_steer > 0:
-          self.steer_torque_over_timer = 0
-        else:
-          #sec_mval = 1
-          self.steer_torque_over_timer = 50       
-      else:
-        self.steer_torque_over_timer = 50
+      if path_plan.laneChangeState != LaneChangeState.off:
+        self.steer_torque_over_timer = 0
+      elif CS.out.leftBlinker or CS.out.rightBlinker:
+        sec_mval = 0.5 # 오파 => 운전자.
+        sec_pval = 10  # 운전자 => 오파  (sec)      
+    else:
+      sec_mval = 0.5  # 오파 => 운전자.  (sec)
+      sec_pval = 10 #  운전자 => 오파  (sec)    
 
-    elif self.steer_torque_over_timer:
+
+
+
+    if self.steer_torque_over_timer:
       self.steer_torque_over_timer -= 1
 
     ratio_pval = 1/(100*sec_pval)
@@ -241,7 +236,7 @@ class CarController():
 
 
     # disable if steer angle reach 90 deg, otherwise mdps fault in some models
-    lkas_active = enabled and abs(CS.out.steeringAngleDeg) < CS.CP.maxSteeringAngleDeg
+    lkas_active = enabled   #and abs(CS.out.steeringAngleDeg) < CS.CP.maxSteeringAngleDeg
 
     if not lkas_active:
       apply_steer = 0
